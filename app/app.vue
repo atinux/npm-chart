b32e8c23730c3a26d2a4b624e7bb743f73efc1af
Sébastien Chopin <seb@nuxt.com>
2024-09-18 17:36:39 +0200

    chore: update meta for google

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,7 @@ useHead({
   },
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+    { name: 'google-site-verification', content: 'YuBf6_cS6BEqCqFpYg8txaJvOjLFoVTv7TK4gUG_S2Q' },
   ],
 })
 useSeoMeta({
