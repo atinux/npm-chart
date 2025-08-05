cc3bd2affc117773d63522167c24ee58aca90968
Sébastien Chopin <atinux@gmail.com>
2025-08-05 23:11:48 +0200

    chore: upgrade to Nuxt v4 and UI v3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-30',
-  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
-  future: { compatibilityVersion: 4 },
 
   // https://nuxt.com/modules
   modules: ['@nuxthub/core', '@nuxt/eslint', '@nuxt/ui', '@nuxt/fonts', '@vueuse/nuxt', '@nuxtjs/plausible', 'nuxt-og-image'],
 
+  // https://devtools.nuxt.com
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2024-07-30',
+
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
     // https://hub.nuxt.com/docs/features/cache
-    cache: true
-  },
-
-  icon: {
-    clientBundle: {
-      scan: true
-    },
+    cache: true,
   },
 
   // https://eslint.nuxt.com
   eslint: {
     config: {
       stylistic: {
-        quotes: 'single'
-      }
-    }
+        quotes: 'single',
+      },
+    },
   },
 
-  // https://devtools.nuxt.com
-  devtools: { enabled: false }
+  icon: {
+    clientBundle: {
+      scan: true,
+    },
+  },
 })
